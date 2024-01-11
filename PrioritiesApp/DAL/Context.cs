afd1e22cb48a9f0aa1b59a47882690bf3c486bd9
david-o-rosario1201 <finnnn023@gmail.com>
2024-01-11 08:29:54 -0400

    Creacion del Modelo y Contexto

---
+++ PrioritiesApp/DAL/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrioritiesApp.Models;
+
+namespace PrioritiesApp.DAL
+{
+	public class Context : DbContext
+	{
+		public Context(DbContextOptions<Context> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Priorities> Priorities { get; set; }
+	}
+}
---
+++ PrioritiesApp/Models/Priorities.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrioritiesApp.Models
+{
+	public class Priorities
+	{
+		[Key]
+		public int PriorityId { get; set; }
+
+		[Required(ErrorMessage = "Error, campo obligatorio")]
+		public string Description { get; set; }
+
+		[Required(ErrorMessage = "Error, campo obligatorio")]
+		public int DaysCommitment { get; set; }
+	}
+}
