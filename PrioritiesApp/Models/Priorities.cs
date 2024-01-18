9cab27cd45364aca4e9bc37b81cf90dcfb101d87
david-o-rosario1201 <finnnn023@gmail.com>
2024-01-17 20:59:00 -0400

    Modificacion a la clase models

--- PrioritiesApp/Models/Priorities.cs
+++ PrioritiesApp/Models/Priorities.cs
@@ -1,17 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace PrioritiesApp.Models
+namespace PrioritiesApp.Models;
+
+public class Priorities
 {
-	public class Priorities
-	{
-        [Key]
-        public int PriorityId { get; set; }
+    [Key]
+    public int PriorityId { get; set; }
 
-		[Required(ErrorMessage = "ERROR, CAMPO OBLIGATORIO")]
-		public string Description { get; set; }
+    [Required(ErrorMessage = "ERROR, CAMPO OBLIGATORIO")]
+    public string Description { get; set; }
 
-        [Range(1, 31, ErrorMessage = "ERROR, CAMPO OBLIGATORIO, RANGO [1-31]")]
-        [Required(ErrorMessage = "ERROR, CAMPO OBLIGATORIO")]
-        public int DaysCommitment { get; set; }
-	}
+    [Range(1, 31, ErrorMessage = "ERROR, CAMPO OBLIGATORIO, RANGO [1-31]")]
+    public int DaysCommitment { get; set; }
 }
