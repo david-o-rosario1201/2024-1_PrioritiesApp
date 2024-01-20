8652443eca0c23926e7d98354ce48b406d522125
david-o-rosario1201 <finnnn023@gmail.com>
2024-01-20 17:09:28 -0400

    Arregle las validaciones que estaban mal puestas y agregue que el RNC sea exactamente 13 caracteres

--- PrioritiesApp/Models/Clientes.cs
+++ PrioritiesApp/Models/Clientes.cs
@@ -9,16 +9,21 @@ public class Clientes
     [Required (ErrorMessage = "Error, debe ingresar un nombre")]
     public string? Nombre { get; set; }
 
-    [Phone(ErrorMessage = "Error, el formato del numero de telefono no es valido"), Required(ErrorMessage = "Error, debe ingresar un numero de teléfono")]
+    [Phone(ErrorMessage = "Error, el formato del numero de telefono no es valido")]
+    [Required(ErrorMessage = "Error, debe ingresar un numero de teléfono")]
     public string? Telefono { get; set; }
 
-    [Phone (ErrorMessage = "Error, el formato del numero de telefono no es valido"), Required(ErrorMessage = "Error, debe ingresar un numero de celular")]
+    [Phone (ErrorMessage = "Error, el formato del numero de telefono no es valido")]
+    [Required(ErrorMessage = "Error, debe ingresar un numero de celular")]
     public string? Celular { get; set; }
 
-    [Required (ErrorMessage = "Error, debe ingresar un RNC")]
-    public string? RNC { get; set; }
+	[Required(ErrorMessage = "Error, debe ingresar un RNC")]
+    [StringLength(11, MinimumLength = 11, ErrorMessage = "El RNC debe tener exactamente 11 caracteres")]
+	public string RNC { get; set; }
 
-    [EmailAddress (ErrorMessage = "Error, el formato del email no es valido"), Required(ErrorMessage = "Error, debe un ingresar un email")]
+
+	[EmailAddress (ErrorMessage = "Error, el formato del email no es valido")]
+    [Required(ErrorMessage = "Error, debe un ingresar un email")]
     public string? Email { get; set; }
 
     [Required (ErrorMessage = "Error, debe una dirección")]
