02db5df2126071981565f488baf7b91a802be445
david-o-rosario1201 <finnnn023@gmail.com>
2024-01-28 16:02:59 -0400

    Hice los services de Sistemas y Tickets

--- PrioritiesApp/Services/SistemasServices.cs
+++ PrioritiesApp/Services/SistemasServices.cs
@@ -3,69 +3,68 @@ using PrioritiesApp.DAL;
 using PrioritiesApp.Models;
 using System.Linq.Expressions;
 
-namespace PrioritiesApp.Services
+namespace PrioritiesApp.Services;
+
+public class SistemasServices
 {
-	public class SistemasServices
+	private readonly Context _context;
+
+	public SistemasServices(Context context)
 	{
-		private readonly Context _context;
+		_context = context;
+	}
 
-        public SistemasServices(Context context)
-        {
-            _context = context;
-        }
+	public async Task<bool> Crear(Sistemas sistema)
+	{
+		if (!await Existe(sistema.SistemaId))
+			return await Insertar(sistema);
+		else
+			return await Modificar(sistema);
+	}
 
-        public async Task<bool> Crear(Sistemas sistema)
-        {
-            if (!await Existe(sistema.SistemaId))
-                return await Insertar(sistema);
-            else
-                return await Modificar(sistema);
-        }
+	public async Task<bool> Insertar(Sistemas sistema)
+	{
+		_context.Sistemas.Add(sistema);
+		return await _context.SaveChangesAsync() > 0;
+	}
 
-        public async Task<bool> Insertar(Sistemas sistema)
-        {
-            _context.Sistemas.Add(sistema);
-            return await _context.SaveChangesAsync() > 0;
-        }
+	public async Task<bool> Modificar(Sistemas sistema)
+	{
+		_context.Update(sistema);
+		var modifico = await _context.SaveChangesAsync() > 0;
+		_context.Entry(sistema).State = EntityState.Detached;
+		return modifico;
+	}
 
-        public async Task<bool> Modificar(Sistemas sistema)
-        {
-			_context.Update(sistema);
-			var modifico = await _context.SaveChangesAsync() > 0;
-			_context.Entry(sistema).State = EntityState.Detached;
-			return modifico;
-		}
+	public async Task<bool> Existe(int id)
+	{
+		return await _context.Sistemas.AnyAsync(s => s.SistemaId == id);
+	}
 
-        public async Task<bool> Existe(int id)
-        {
-            return await _context.Sistemas.AnyAsync(s => s.SistemaId == id);
-        }
+	public async Task<bool> Eliminar(Sistemas sistema)
+	{
+		var cantidad = await _context.Sistemas
+			.Where(s => s.SistemaId == sistema.SistemaId)
+			.ExecuteDeleteAsync();
+		return cantidad > 0;
+	}
 
-        public async Task<bool> Eliminar(Sistemas sistema)
-        {
-            var cantidad = await _context.Sistemas
-                .Where(s => s.SistemaId == sistema.SistemaId)
-                .ExecuteDeleteAsync();
-            return cantidad > 0;
-        }
+	public async Task<Sistemas?> BuscarId(int Id)
+	{
+		return await _context.Sistemas
+			.AsNoTracking()
+			.FirstOrDefaultAsync(p => p.SistemaId == Id);
+	}
 
-		public async Task<Sistemas?> BuscarId(int Id)
-		{
-			return await _context.Sistemas
-				.AsNoTracking()
-				.FirstOrDefaultAsync(p => p.SistemaId == Id);
-		}
+	public async Task<Sistemas?> BuscarSistema(string nombre)
+	{
+		return await _context.Sistemas
+			.AsNoTracking()
+			.FirstOrDefaultAsync(s => s.Nombre == nombre);
+	}
 
-        public async Task<Sistemas?> BuscarSistema(string nombre)
-        {
-            return await _context.Sistemas
-                .AsNoTracking()
-                .FirstOrDefaultAsync(s => s.Nombre == nombre);
-        }
-
-		public async Task<List<Sistemas>> Listar(Expression<Func<Sistemas, bool>> criterio)
-        {
-            return await _context.Sistemas.AsNoTracking().Where(criterio).ToListAsync();
-        }
-    }
+	public async Task<List<Sistemas>> Listar(Expression<Func<Sistemas, bool>> criterio)
+	{
+		return await _context.Sistemas.AsNoTracking().Where(criterio).ToListAsync();
+	}
 }
---
+++ PrioritiesApp/Services/TicketsServices.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrioritiesApp.DAL;
+using PrioritiesApp.Models;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace PrioritiesApp.Services;
+
+public class TicketsServices
+{
+	private readonly Context _context;
+
+	public TicketsServices(Context context)
+	{
+		_context = context;
+	}
+
+	public async Task<bool> Crear(Tickets tickets)
+	{
+		if (!await Existe(tickets.TicketId))
+			return await Insertar(tickets);
+		else
+			return await Modificar(tickets);
+	}
+
+	public async Task<bool> Insertar(Tickets tickets)
+	{
+		_context.Tickets.Add(tickets);
+		return await _context.SaveChangesAsync() > 0;
+	}
+
+	public async Task<bool> Modificar(Tickets tickets)
+	{
+		_context.Update(tickets);
+		var modifico = await _context.SaveChangesAsync() > 0;
+		_context.Entry(tickets).State = EntityState.Detached;
+		return modifico;
+	}
+
+	public async Task<bool> Existe(int id)
+	{
+		return await _context.Tickets.AnyAsync(t => t.TicketId == id);
+	}
+
+	public async Task<bool> Eliminar(Tickets tickets)
+	{
+		var cantidad = await _context.Tickets
+			.Where(t => t.TicketId == tickets.TicketId)
+			.ExecuteDeleteAsync();
+		return cantidad > 0;
+	}
+
+	public async Task<Tickets?> BuscarId(int Id)
+	{
+		return await _context.Tickets
+			.AsNoTracking()
+			.FirstOrDefaultAsync(t => t.TicketId == Id);
+	}
+
+	/*public async Task<Tickets?> BuscarSistema(string nombre)
+	{
+		return await _context.Tickets
+			.AsNoTracking()
+			.FirstOrDefaultAsync(t => t.Nombre == nombre);
+	}*/
+
+	public async Task<List<Tickets>> Listar(Expression<Func<Tickets, bool>> criterio)
+	{
+		return await _context.Tickets.AsNoTracking().Where(criterio).ToListAsync();
+	}
+}
