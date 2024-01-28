207f7b2d88bb439962dafc78dcdfa7c875ca4972
david-o-rosario1201 <finnnn023@gmail.com>
2024-01-28 01:48:01 -0400

    Cree el modelo de Sistemas

---
+++ PrioritiesApp/Models/Sistemas.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrioritiesApp.Models
+{
+	public class Sistemas
+	{
+		[Key]
+		public int SistemaId { get; set; }
+
+		[Required(ErrorMessage = "Error, debe incluir un nombre")]
+		public string Nombre { get; set; }
+	}
+}
