3f003def8009d18f5683b5904d8576f964ac3956
david-o-rosario1201 <finnnn023@gmail.com>
2024-01-29 10:49:46 -0400

    Arregle la identacion en el modelo Tickets

--- PrioritiesApp/Models/Tickets.cs
+++ PrioritiesApp/Models/Tickets.cs
@@ -1,36 +1,35 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace PrioritiesApp.Models
+namespace PrioritiesApp.Models;
+
+public class Tickets
 {
-	public class Tickets
-	{
-		[Key]
-		public int TicketId { get; set; }
+	[Key]
+	public int TicketId { get; set; }
 
-		[Required(ErrorMessage = "Error, la fecha es requerida")]
-		[DataType(DataType.Date)]
-		public DateTime Fecha { get; set; }
+	[Required(ErrorMessage = "Error, la fecha es requerida")]
+	[DataType(DataType.Date)]
+	public DateTime Fecha { get; set; }
 
-		[ForeignKey("Clientes")]
-		[Required(ErrorMessage = "Error, debe llenar este campo")]
-		public int ClienteId { get; set; }
+	[ForeignKey("Clientes")]
+	[Required(ErrorMessage = "Error, debe llenar este campo")]
+	public int ClienteId { get; set; }
 
-		[ForeignKey("Sistemas")]
-		[Required(ErrorMessage = "Error, debe llenar este campo")]
-		public int SistemaId { get; set; }
+	[ForeignKey("Sistemas")]
+	[Required(ErrorMessage = "Error, debe llenar este campo")]
+	public int SistemaId { get; set; }
 
-		[ForeignKey("Priorities")]
-		[Required(ErrorMessage = "Error, debe llenar este campo")]
-		public int PriorityId { get; set; }
+	[ForeignKey("Priorities")]
+	[Required(ErrorMessage = "Error, debe llenar este campo")]
+	public int PriorityId { get; set; }
 
-		[Required(ErrorMessage = "Error, debe llenar este campo")]
-		public string SolicitadoPor { get; set; }
+	[Required(ErrorMessage = "Error, debe llenar este campo")]
+	public string SolicitadoPor { get; set; }
 
-		[Required(ErrorMessage = "Error, debe llenar este campo")]
-		public string Asunto { get; set; }
+	[Required(ErrorMessage = "Error, debe llenar este campo")]
+	public string Asunto { get; set; }
 
-		[Required(ErrorMessage = "Error, debe llenar este campo")]
-		public string Descripcion { get; set; }
-	}
+	[Required(ErrorMessage = "Error, debe llenar este campo")]
+	public string Descripcion { get; set; }
 }
